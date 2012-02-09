e108c2ef4f467b4b42a39be3aeabfeb0228f73ac
algard <david.algar@mindbodyonline.com>
2012-02-09 02:30:03 +0000

    reworked

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var server = require("./server");
+var server = require("./server.js");
 
 server.start();
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var http = require("http");
 
 function start() {
+    var counter;
   function onRequest(request, response) {
     console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    response.write("Hello what's up visitor #" + counter++);
     response.end();
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(process.env.C9_PORT);
   console.log("Server has started.");
 }
 
